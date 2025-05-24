ba341f23eca5337287486598a8089d43df280c86
JeremyBoddy <jeremybodd@gmail.com>
2025-05-24 08:53:22 -0500

    pro.4 final stage

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,47 @@
 const ediProfileButton = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
-const editProfileCloseButton = editProfileModal.querySelector(".modal__close-button");
+const editProfileCloseButton = editProfileModal.querySelector(
+  ".modal__close-button"
+);
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
 
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 const newPostButton = document.querySelector(".profile__new-post-button");
 const newPostmodal = document.querySelector("#new-post-modal");
-const newPostCloseButton= newPostmodal.querySelector("modal__close-button");
+const newPostCloseButton = newPostmodal.querySelector("modal__close-button");
 
-editProfileCloseButton.addEventListener("click", function (){
-    editProfileModal.classList.add("modal_is-opened");
+const profileNameEl = document.querySelector(".profile__name");
+const profileDescriptionEl = document.querySelector(".profile__desription");
+
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameEl.textContent;
+  //TODO-fill descripton input
+  editProfileModal.classList.add("modal_is-opened");
 });
 
-editProfileCloseButton.addEventListener("click", function (){
-    editProfileModal.classList.remove("modal_is-opened");
+editProfileCloseButton.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
 });
 
-newPostButton.addEventListener("click", function (){
-    newPostModal.classList.add("modal_is-opened");
+newPostButton.addEventListener("click", function () {
+    newPost
+  newPostModal.classList.add("modal_is-opened");
 });
 
-newPostCloseButton.addEventListener("click", function (){
-    newPostModal.classList.remove("modal_is-opened");
+newPostCloseButton.addEventListener("click", function () {
+  newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameEl.textContent = editProfileNameInput.value;
+  //TODO - handle other input
+  editProfileModal.classList.remove("modal_is-opened");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
